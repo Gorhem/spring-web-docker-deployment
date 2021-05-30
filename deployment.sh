c08d1397913902e694c0f761e7141f6ba9142fd6
gorhem <gorhemsaka@gmail.com>
2021-05-30 19:23:42 +0300

    cleaned

--- deployment.sh
+++ deployment.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-aws ecr get-login-password --region us-east-2 | sudo docker login --username AWS --password-stdin 257564702288.dkr.ecr.us-east-2.amazonaws.com
-sudo docker pull 257564702288.dkr.ecr.us-east-2.amazonaws.com/spring-web:latest
-sudo docker run --name my-container -d -p 8080:8080 257564702288.dkr.ecr.us-east-2.amazonaws.com/spring-web:latest
+aws ecr get-login-password --region us-east-2 | sudo docker login --username AWS --password-stdin {this-part-unique-to-you}.dkr.ecr.us-east-2.amazonaws.com
+sudo docker pull {this-part-unique-to-you}.dkr.ecr.us-east-2.amazonaws.com/spring-web:latest
+sudo docker run --name my-container-name -p 8080:8080 -d {this-part-unique-to-you}.dkr.ecr.us-east-2.amazonaws.com/spring-web:latest
